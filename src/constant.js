2cefde0de8954c529e8113315a25af3e64b998b3
Maryam Abdulle <iammaryamabdulle@gmail.com>
2022-12-29 23:44:38 -0700

    added

---
+++ src/constant.js
@@ -0,0 +1,5 @@
+export const EMAIL_ID           = 'iammaryamabdulle@gmail.com';
+
+export const RESUME_LINK        = ' https://drive.google.com/file/d/1D3qCC4Xsol3pDiPXYZuY8d-dXchQL8Hi/view';
+export const GITHUB_LINK        = 'https://github.com/Maryamabdulle';
+export const LINKEDIN_LINK      = 'https://www.linkedin.com/in/maryamabdulle/';
